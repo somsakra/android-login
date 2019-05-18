b62551c96194ae1fccec6511bcae2381e177a1c4
somsakra <somsakra@live.com>
2019-05-18 12:07:19 +0700

    test2

---
+++ includes/Constants.php
@@ -0,0 +1,5 @@
+<?php
+    define('DB_NAME','android');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    defind('DB_HOST','localhost');
---
+++ includes/DbOperation.php
@@ -0,0 +1,28 @@
+<?php
+
+    class DbOperation {
+
+        private $con;
+
+        function __construct(){
+            require_once dirname(__FILE__).'/DbConnnect.php';
+
+            $db = new DbConnect();
+
+            $this->con = $db->connect();
+        }
+        /*Create User */
+        function createUser($username, $pass, $email){
+            $password = md5($pass);
+            $stmt = this->con->prepare("INSERT INTO `users` (`id`, `username`, 
+            `password`, `email`) VALUES (NULL, ?, ?, ?);");
+            $stmt->bind_param("sss",$username,$password,$email);
+
+            if($stmt->execute()){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }
+    } 
