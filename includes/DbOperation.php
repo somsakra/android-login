1fe7e72a4ccd37d063a7b473d0c17dce2dbad9cb
somsakra <somsakra@live.com>
2019-05-18 12:21:53 +0700

    test

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -14,8 +14,7 @@
         /*Create User */
         function createUser($username, $pass, $email){
             $password = md5($pass);
-            $stmt = this->con->prepare("INSERT INTO `users` (`id`, `username`, 
-            `password`, `email`) VALUES (NULL, ?, ?, ?);");
+            $stmt = this->con->prepare("INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, ?, ?, ?);");
             $stmt->bind_param("sss",$username,$password,$email);
 
             if($stmt->execute()){
